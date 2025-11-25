e9ce6301b8f4bd836e2fc85adc6c396f85bee8a3
Navith-25 <wwnavith@gmail.com>
2025-11-25 17:06:19 +0530

    Updated Navith Mail Counter code

--- Navith_IR.ino/Navith_IR.ino.ino
+++ Navith_IR.ino/Navith_IR.ino.ino
@@ -1,19 +1,48 @@
+#include <WiFi.h>
+#include <FirebaseESP32.h>
+#include <DHT.h>
 
-const int IR_SENSOR_PIN = 25; 
+#define WIFI_SSID "OPPO A78"
+#define WIFI_PASSWORD "12345678"
+#define API_KEY "AIzaSyDCf1MiexBrF9hViJDh4nhiEQBhRUqPAkk"
+#define DATABASE_URL "test-2-ee62f-default-rtdb.asia-southeast1.firebasedatabase.app/"
+#define USER_EMAIL "ashenharshana02@gmail.com"
+#define USER_PASSWORD "Ashen@fdo10"
+
+#define DHT_PIN 21
+#define DHTTYPE DHT11
+
+DHT dht(DHT_PIN, DHTTYPE);
+FirebaseData fbdo;
+FirebaseAuth auth;
+FirebaseConfig config;
 
 void setup() {
-  Serial.begin(115200); 
-  Serial.println("ESP32 IR Reflective Sensor Test Started...");
-  pinMode(IR_SENSOR_PIN, INPUT);
+  Serial.begin(115200);
+  dht.begin();
+
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) { delay(300); Serial.print("."); }
+  Serial.println("\nConnected.");
+
+  config.api_key = API_KEY;
+  config.database_url = DATABASE_URL;
+  auth.user.email = USER_EMAIL;
+  auth.user.password = USER_PASSWORD;
+  Firebase.begin(&config, &auth);
+  Serial.println("Ridma's System Ready.");
 }
 
 void loop() {
-  int sensorState = digitalRead(IR_SENSOR_PIN);
-  if (sensorState == LOW) {
-    Serial.println("MAIL DETECTED! (Object reflected the signal)");
-  } else {
-    Serial.println("Slot Clear.");
-  }
+  delay(5000);
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
 
-  delay(100); 
+  if (!isnan(h) && !isnan(t)) {
+    Serial.print("Temp: "); Serial.println(t);
+    Firebase.setFloat(fbdo, "/temperature", t);
+    Firebase.setFloat(fbdo, "/humidity", h);
+  } else {
+    Serial.println("DHT Error");
+  }
 }
